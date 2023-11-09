ac29493f60f60ea9b33daa7b9114f34e07f3db1f
Nikita <barkov.nikitav@gmail.com>
2023-11-09 20:59:01 +0300

    v5

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('blog.urls')),
-    path('posts/', include('blog.urls')),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
